88883f4c656bdf00c46543ca84e3d0591ee280ea
William <williamliususanto@gmail.com>
2025-02-27 16:03:59 +0700

    Add example unit tests

---
+++ utils/for_testing.js
@@ -0,0 +1,22 @@
+const reverse = (string) => {
+  return string
+    .split('')
+    .reverse()
+    .join('')
+}
+
+const average = (array) => {
+  if (array === null || array === undefined || array.length === 0) {
+    return 0
+  }
+
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+  reverse,
+  average
+}
